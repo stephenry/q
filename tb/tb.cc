953a2f9e7b0e9c9b769d297488453a8e20e77c74
Stephen Henry <stephenry@gmail.com>
2021-03-28 12:05:40 +0100

    Revert "Migrate to Verilator 4.200"
    This reverts commit 35832f3fdb3a571764c1e1a24231ee684b781f04.
    
    Ubuntu 20.04-based Continuous Integration environment does not
    yet have most recent Verilator version.

--- tb/tb.cc
+++ tb/tb.cc
@@ -702,13 +702,12 @@ void Model::Arch::rf_write(vluint8_t a, vluint32_t d) {
 
 TB::TB(const Options& opts)
     : opts_(opts) {
-  ctxt_ = new VerilatedContext;
 #ifdef OPT_VCD_ENABLE
   if (opts.wave_enable) {
-    ctxt_->traceEverOn(true);
+    Verilated::traceEverOn(true);
   }
 #endif
-  u_ = new Vtb_qs(ctxt_, "top");
+  u_ = new Vtb_qs;
   vs_ = VSignals::bind(u_);
 #ifdef OPT_VCD_ENABLE
   if (opts.wave_enable) {
@@ -724,7 +723,6 @@ TB::TB(const Options& opts)
 
 TB::~TB() {
   delete u_;
-  delete ctxt_;
 #ifdef OPT_VCD_ENABLE
   if (wave_) {
     wave_->close();
